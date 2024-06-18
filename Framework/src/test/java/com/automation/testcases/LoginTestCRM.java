626e945dafe16f9aee47ac26cf09614fb4046690
shaloo <shaloogupta531995@gmail.com>
2024-06-18 13:46:26 +0530

    updated

--- Framework/src/test/java/com/automation/testcases/LoginTestCRM.java
+++ Framework/src/test/java/com/automation/testcases/LoginTestCRM.java
@@ -7,9 +7,8 @@ import org.openqa.selenium.support.PageFactory;
 
 import com.automation.pages.BaseClass;
 import com.automation.pages.LoginPage;
-import com.automation.utility.RerunAutomationScript;
 
-@Test(retryAnalyzer = RerunAutomationScript.class)
+@Test
 public class LoginTestCRM extends BaseClass {
 	
 	public void verifyUserAbleToLaunchTheURL() throws InterruptedException {
@@ -18,7 +17,7 @@ public class LoginTestCRM extends BaseClass {
 		looger.info("url launched");
 		loginPage.clickOnLoginButton();
 		loginPage.loginToCRM(excelReader.getDataFromExcelSheet(0,0),excelReader.getDataFromExcelSheet(0,1));
-		assertTrue(loginPage.geterrorMessageWhileLogIn().contains("Invalidlogin"), "Verify the error mesaage.");
+		assertTrue(loginPage.geterrorMessageWhileLogIn().contains("Invalid login"), "Verify the error mesaage.");
 		looger.pass("login success");
 		
 	}
